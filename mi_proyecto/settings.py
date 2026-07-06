10d8a59ed028f4597c693c4962a40feddbf5cad1
Juan Carlos <juancarloshdzalv21@gmail.com>
2026-07-06 09:56:45 -0600

    Completa Docker PostgreSQL modelos diagramas y mockups

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -56,11 +56,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'firmar_documentos'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
@@ -87,6 +90,5 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 STATIC_URL = 'static/'
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
